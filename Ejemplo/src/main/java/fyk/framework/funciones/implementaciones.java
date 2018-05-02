a44ef80a2935f7539ae0c09abcdb294a93112831
Franco Battaglia <francobatta@outlook.com>
2018-05-02 04:02:06 -0300

    Mejorado todo
    Adecuado a String
    Agregados mas metodos para trabajar con el objeto recuperado
    etc

--- Ejemplo/src/main/java/fyk/framework/funciones/implementaciones.java
+++ Ejemplo/src/main/java/fyk/framework/funciones/implementaciones.java
@@ -1,5 +1,6 @@
 package fyk.framework.funciones;
 import java.io.FileInputStream;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
 import java.sql.*;
 import java.util.Properties;
@@ -18,26 +19,31 @@ public class implementaciones
 	// funcion que retorna una instancia de T si se encuentra en la fila buscada
 	//ESTO ANDA
 	public static <T> T find(Class<T> instancia, Object id) throws ClassNotFoundException, SQLException{
-		Object clave = obtenerCampoId(instancia);
-		String hql = String.format("SELECT * FROM %s WHERE %s=%s",instancia,clave,id);
-		Connection conn = hacerConexion();
+		String nombreDeLaTabla = instancia.getSimpleName(); // obtiene el nombre de la clase (tabla) en String
+		String clave = obtenerCampoId(instancia); // obtiene cual es el nombre del campo que contiene la clave
+		String idString = String.valueOf(id); // convierte el id que pasaron a String para asegurar igualdad
+		String hql = String.format("SELECT * FROM %s WHERE %s=%s",nombreDeLaTabla,clave,idString); // string que va a ser pasado como query
+		
+		Connection conn = hacerConexion(); // conecta con la base de datos
 		Statement stmt = conn.createStatement();
-		ResultSet rs = stmt.executeQuery(hql);
+		ResultSet rs = stmt.executeQuery(hql); // manda query al motor
+		
+		// creacion del objeto nuevo:
+        try {
+			
+      		Constructor<T> constructorSinParametros = instancia.getConstructor(); // agarra constructor	
+      		Object nuevoObjetoDeMiClase= (Object) constructorSinParametros.newInstance(); // instancia el objeto a partir de constructor
+      	
+      		Field[] listaAtributos = instancia.getDeclaredFields(); // array de atributos
+      		Annotation[] anotaciones = instancia.getAnnotations();	// array de annotations
+
+       		System.out.println("\nNuevo objeto creado");
+
+       	} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | SecurityException | NoSuchMethodException e) {
+       		e.printStackTrace();
+       	}
 		while(rs.next()){
-	         // crear instancia con lo encontrado?
-	         Field[] listaAtributos = instancia.getFields();
-	         try {
-	        		Constructor<T> constructorSinParametros = instancia.getConstructor(); // agarra que clase es
-	        				
-	        		Object nuevoObjetoDeMiClase=constructorSinParametros.newInstance(); // instancia el objeto
-
-	        		// faltaria setear todos los atributos
-
-	        		System.out.println("\nNuevo objeto creado");
-
-	        	} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | SecurityException | NoSuchMethodException e) {
-	        		e.printStackTrace();
-	        	}
+			// mapear los resultados del SQL al objeto creado...
 	      }
 	      // cierre de la conexion
 	      rs.close();
@@ -46,16 +52,16 @@ public class implementaciones
 		return null;
 	}
 	
-	public static <T> Object obtenerCampoId(Class<T> instancia){
-		Field[] fields = instancia.getFields(); // agrupa todos los atributos de la clase en una coleccion
+	public static <T> String obtenerCampoId(Class<T> instancia){
+		Field[] fields = instancia.getDeclaredFields(); // agrupa todos los atributos de la clase en una coleccion
 		for (Field f : fields) // para todos los atributos de la clase deseada:
 		{
 			if (f.isAnnotationPresent(Id.class)) // si el atributo tiene annotation id...
 			{
-				return f; // retorna el campo clave
+				return f.getName(); // retorna NOMBRE del campo clave
 			}
 	}
-		return -1;
+		return "Tirar error";
 }
 	public static Connection hacerConexion() throws SQLException, ClassNotFoundException{
 		Properties props = new Properties();
